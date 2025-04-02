f474e37cade4d1425d49d37a7b0580fdb3460d65
studentdebtcollective <info@studentdebtcollective.com>
2025-04-02 18:49:24 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 window.onload = function () {
-    jQuery("#numSubs").html('23');
+    jQuery("#numSubs").html('36');
     jQuery('.box .percent svg circle:nth-child(2)').css('stroke-dashoffset','calc(440 - (440 * 23) / 100)');
+
+    //insta 2
+    //tiktk 0
+    //faceb 34
+    //sinup 0
  }
             
         
